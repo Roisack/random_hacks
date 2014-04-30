7b47876d4df0fe42f9c59d0fcc7d7bd1e6140a7e
Jesse Kaukonen <jesse.kaukonen@gmail.com>
2014-04-30 15:10:18 +0300

    Replacing old popen with subprocess, error handling as apparently its hard to get data from the service these days

--- weather/do_report.py
+++ weather/do_report.py
@@ -1,5 +1,8 @@
 import os
 import shutil
+import time
+import subprocess
+from subprocess import Popen, PIPE, STDOUT
 
 def write_row(data, place):
     f = open('./report.html', 'a')
@@ -39,6 +42,14 @@ def write_row(data, place):
 
     f.close()
 
+def write_error(city):
+    f = open('./report.html', 'a')
+    f.write("<tr><td>")
+    f.write(city)
+    f.write("</td><td>No data received")
+    f.write("</td><td></td><td></td><td></td></tr>")
+    f.close()
+
 def finish_document():
     print("Finishing document")
     f = open('./report.html', 'a')
@@ -57,20 +68,37 @@ def finish_document():
 
 
 def fetch_data(code): 
-    cmdline = "./weather.sh "
-    cmdline += code
-    process = os.popen(cmdline, "r")
-    output = ""
-    while 1:
-        line = process.readline()
-        if not line: break
-        output += line
+    cmdline = ["/usr/bin/weather", code]
+
+    output = None
+    try:
+        output = subprocess.check_output(cmdline)
+    except Exception as e:
+        raise Exception("Error retrieving data")
+    print("Output is:")
+    print(output)
     return output
 
 def do_report(city, code):
-    output = fetch_data(code)
-    print("Writing data for " + city)
+    trying = True
+    tries = 1
+    output = None
+    while (trying == True):
+        try:
+            print("Fetching data (#attempt " + str(tries) + ")")
+            output = fetch_data(code)
+            trying = False
+        except Exception as e:
+            print(e)
+            tries += 1
+            if (tries >= 4):
+                trying = False
+                print("Giving up after 4 tries. Writing error to output")
+                write_error(city)
+                return
+
     write_row(output, city)
+    time.sleep(1)
 
 def prepare():
     try:
@@ -88,14 +116,23 @@ def upload():
         print(e)
 
 prepare()
-do_report("Turku", "EFTU")
-do_report("Kuopio", "EFTU")
-do_report("Clarksville", "KCKV")
-do_report("North Vancouver", "CYVR")
-do_report("Detroit", "KDTW")
-do_report("Miami", "KMIA")
-do_report("Quebec", "CYQB")
-do_report("Sao Paolo", "SBGR")
+do_report("Clarksville (Ranti)", "KCKV")
+do_report("Turku (Gekko)", "EFTU")
+do_report("Tampere", "EFTP")
+do_report("Kuopio", "EFKU")
+do_report("Oulu", "EFOU")
+do_report("Helsinki", "EFHK")
+do_report("Moscow (Xai)", "UUEE")
+do_report("Palanga (Rytis)", "EYPA")
+do_report("Kansas City (Dritz)", "KMCI")
+do_report("North Vancouver (AmonX)", "CYVR")
+do_report("Detroit (Oldlaptop)", "KDTW")
+do_report("Miami (Ziper)", "KMIA")
+do_report("Bagotville (Blue)", "CYBG")
+do_report("Florianopolis (Yohan)", "SBFL")
+do_report("Chennai", "VOMM")
+do_report("Dusseldorf (Vrad)", "EDDL")
+do_report("Melbourne (chaos95)", "YMML")
 finish_document()
 upload()
 
